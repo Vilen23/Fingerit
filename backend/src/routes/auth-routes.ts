945b3d5a33282b8b4d8fadf7557d68366b1fc643
Vilen23 <shivamlather5@gmail.com>
2024-06-17 22:56:38 +0530

    Added google route

--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,8 +1,9 @@
-import express from "express"
-import { signin, signup } from "../controllers/auth-controller";
+import express from "express";
+import { googleSignup, signin, signup } from "../controllers/auth-controller";
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signup/google", googleSignup);
 
-export default router
+export default router;
