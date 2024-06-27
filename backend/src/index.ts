ef364aef87f699f4476b58d50014be0c82234c4d
Vilen23 <shivamlather5@gmail.com>
2024-06-27 12:42:08 +0530

    Checking logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,150 +30,158 @@ wss.on("connection", (ws: CustomWebSocket) => {
 
   ws.on("message", async (message) => {
     const { action, payload } = JSON.parse(message.toString());
-    switch (action) {
-      case "joinRoom":
-        const { roomId, userId, word } = payload;
-        if (!rooms[roomId]) {
-          rooms[roomId] = new Set();
-        }
-        rooms[roomId].add(ws);
-        ws.roomId = roomId;
-        ws.userId = userId;
-        ws.speed = 0;
+    try {
+      switch (action) {
+        case "joinRoom":
+          const { roomId, userId, word } = payload;
+          if (!rooms[roomId]) {
+            rooms[roomId] = new Set();
+          }
+          rooms[roomId].add(ws);
+          ws.roomId = roomId;
+          ws.userId = userId;
+          ws.speed = 0;
 
-        console.log(`User ${userId} joined room ${roomId}`);
+          console.log(`User ${userId} joined room ${roomId}`);
 
-        const roomusers = await db.roomUser.findMany({
-          where: {
-            roomId: roomId,
-          },
-        });
+          const roomusers = await db.roomUser.findMany({
+            where: {
+              roomId: roomId,
+            },
+          });
 
-        console.log(`Room users for room ${roomId}:`, roomusers);
+          console.log(`Room users for room ${roomId}:`, roomusers);
 
-        const room = await db.room.findFirst({
-          where: {
-            id: roomId,
-          },
-        });
-
-        console.log(`Room details for room ${roomId}:`, room);
-
-        if (room?.RoomOwnerId === ws.userId) {
-          ws.words = word;
-          const gametext = await db.room.update({
+          const room = await db.room.findFirst({
             where: {
               id: roomId,
             },
-            data: {
-              gametext: word,
+          });
+
+          console.log(`Room details for room ${roomId}:`, room);
+
+          if (room?.RoomOwnerId === ws.userId) {
+            ws.words = word;
+            const gametext = await db.room.update({
+              where: {
+                id: roomId,
+              },
+              data: {
+                gametext: word,
+              },
+              select: {
+                gametext: true,
+              },
+            });
+
+            console.log(`Updated gametext for room ${roomId}:`, gametext);
+          }
+
+          const gametext = await db.room.findFirst({
+            where: {
+              id: roomId,
             },
             select: {
               gametext: true,
             },
           });
 
-          console.log(`Updated gametext for room ${roomId}:`, gametext);
-        }
-
-        const gametext = await db.room.findFirst({
-          where: {
-            id: roomId,
-          },
-          select: {
-            gametext: true,
-          },
-        });
-
-        const usersId = roomusers.map((user: any) => user.userId);
-        const users = await db.user.findMany({
-          where: {
-            id: {
-              in: usersId,
+          const usersId = roomusers.map((user: any) => user.userId);
+          const users = await db.user.findMany({
+            where: {
+              id: {
+                in: usersId,
+              },
             },
-          },
-          select: {
-            id: true,
-            username: true,
-            email: true,
-            RoomOwner: true,
-          },
-        });
+            select: {
+              id: true,
+              username: true,
+              email: true,
+              RoomOwner: true,
+            },
+          });
 
-        console.log(`Users in room ${roomId}:`, users);
+          console.log(`Users in room ${roomId}:`, users);
 
-        if (!rooms[roomId]) return;
-        console.log(rooms[roomId].size);
-        rooms[roomId].forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(
-              JSON.stringify({
-                action: "userJoined",
-                payload: {
-                  users: users,
-                  roomOwner: room?.RoomOwnerId,
-                  room: room,
-                  words: ws.words || gametext?.gametext,
-                },
-              })
-            );
-          }
-        });
-        break;
-      case "start":
-        const roomid = ws.roomId;
-        if (roomid && rooms[roomid]) {
-          rooms[roomid].forEach((client) => {
+          if (!rooms[roomId]) return;
+          console.log(rooms[roomId].size);
+          rooms[roomId].forEach((client) => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ action: "start" }));
+              client.send(
+                JSON.stringify({
+                  action: "userJoined",
+                  payload: {
+                    users: users,
+                    roomOwner: room?.RoomOwnerId,
+                    room: room,
+                    words: ws.words || gametext?.gametext,
+                  },
+                })
+              );
             }
           });
-        }
-        break;
-      case "reload":
-        const roomidReload = ws.roomId;
-        if (roomidReload && rooms[roomidReload]) {
-          rooms[roomidReload].forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ action: "reload" }));
-            }
-          });
-        }
-        break;
-      case "speed":
-        const { speed } = payload;
-        if (speed) {
-          ws.speed = speed;
-        }
-        if (!ws.roomId) return;
-        const usersInChallenge = await db.roomUser.findMany({
-          where: {
-            roomId: ws.roomId,
-          },
-        });
-        const usersChallenge = await db.user.findMany({
-          where: {
-            id: {
-              in: usersInChallenge.map((user: any) => user.userId),
-            },
-          },
-        });
-        const usersInChallengeMap = new Map();
-        usersChallenge.map((user: any) => {
-          usersInChallengeMap.set(user.id, user);
-        });
-        const roomDetails = Array.from(rooms[ws.roomId]).map((client) => ({
-          speed: client.speed,
-          user: usersInChallengeMap.get(client.userId),
-        }));
-        rooms[ws.roomId].forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(
-              JSON.stringify({ action: "speed", payload: roomDetails })
-            );
+          break;
+        case "start":
+          const roomid = ws.roomId;
+          if (roomid && rooms[roomid]) {
+            rooms[roomid].forEach((client) => {
+              if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ action: "start" }));
+              }
+            });
           }
-        });
-        break;
+          break;
+        case "reload":
+          const roomidReload = ws.roomId;
+          if (roomidReload && rooms[roomidReload]) {
+            rooms[roomidReload].forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ action: "reload" }));
+              }
+            });
+          }
+          break;
+        case "speed":
+          const { speed } = payload;
+          if (speed) {
+            ws.speed = speed;
+          }
+          if (!ws.roomId) return;
+          const usersInChallenge = await db.roomUser.findMany({
+            where: {
+              roomId: ws.roomId,
+            },
+          });
+
+          const usersChallenge = await db.user.findMany({
+            where: {
+              id: {
+                in: usersInChallenge.map((user: any) => user.userId),
+              },
+            },
+          });
+
+          const usersInChallengeMap = new Map();
+          usersChallenge.map((user: any) => {
+            usersInChallengeMap.set(user.id, user);
+          });
+
+          const roomDetails = Array.from(rooms[ws.roomId]).map((client) => ({
+            speed: client.speed,
+            user: usersInChallengeMap.get(client.userId),
+          }));
+
+          rooms[ws.roomId].forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(
+                JSON.stringify({ action: "speed", payload: roomDetails })
+              );
+            }
+          });
+          break;
+      }
+    } catch (error) {
+      console.error("Error handling message:", error);
     }
   });
 
